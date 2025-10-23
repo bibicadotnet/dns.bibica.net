b84754b8ecf36e5f183ce422d1d917de4ec75751
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-10-24 01:05:19 +0700

    Update 1_setup-agh-caddy.sh

--- 1_setup-agh-caddy.sh
+++ 1_setup-agh-caddy.sh
@@ -87,3 +87,10 @@ EOF
 hash -r
 
 echo "[+] Installation completed!"
+echo
+cat <<'EOF'
+
+[INFO] Shortcuts available:
+- Use 'caddy start|stop|restart|reload|status|logs' to control Caddy.
+- Use 'agh start|stop|restart|status|install|uninstall' to control AdGuard Home.
+EOF
