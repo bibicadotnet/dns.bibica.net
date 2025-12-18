73f982ad2b07491a88e420e6193cb72fa6a6a159
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-12-18 21:57:41 +0700

    Update windows_auto_config.ps1

--- windows_auto_config.ps1
+++ windows_auto_config.ps1
@@ -389,7 +389,7 @@ WScript.Sleep 1000
 
 ' Start winws
 ws.CurrentDirectory = "C:\dns-bibica-net-doh\zapret"
-ws.Run "winws.exe --wf-tcp=80,443 --wf-udp=443 --hostlist=blacklist.txt --dpi-desync=fake,disorder --dpi-desync-fooling=badseq --dpi-desync-repeats=2", 0, False
+ws.Run "winws.exe --wf-tcp=80,443 --wf-udp=443 --hostlist=blacklist.txt --dpi-desync=fake,disorder --dpi-desync-fooling=badseq --dpi-desync-repeats=1", 0, False
 
 WScript.Sleep 2000
 
