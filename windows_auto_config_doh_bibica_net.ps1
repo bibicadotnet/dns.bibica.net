28240158a9e681f746d3f19b9eeac36e1c6dec16
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-06-16 17:39:04 +0700

    Update windows_auto_config_doh_bibica_net.ps1

--- windows_auto_config_doh_bibica_net.ps1
+++ windows_auto_config_doh_bibica_net.ps1
@@ -416,6 +416,7 @@ sextop1.sale
 youporn.com
 www.wattpad.com
 mangadex.org
+fitgirl-repacks.site
 "@ | Out-File "$zapretPath\blacklist.txt" -Encoding UTF8
 
 # BAT restart script (for manual restart after config changes)
