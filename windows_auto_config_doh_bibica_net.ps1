cd9f85dfe111718b94c45cdce47bb0cc42edb3a8
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-07-08 12:33:00 +0700

    Update windows_auto_config_doh_bibica_net.ps1

--- windows_auto_config_doh_bibica_net.ps1
+++ windows_auto_config_doh_bibica_net.ps1
@@ -426,6 +426,7 @@ www.wattpad.com
 mangadex.org
 fitgirl-repacks.site
 voatiengviet.com
+voz.vn
 "@ | Out-File "$zapretPath\blacklist.txt" -Encoding UTF8
 
 # BAT restart script (for manual restart after config changes)
